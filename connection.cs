dfa27099f80705086ebcbace8d1687c6f7db53e8
Michael Bondi <mbondi@rls2000.com>
2015-01-17 16:00:26 -0500

    First commit

---
+++ connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UfoDB
+{
+    public static class MongoDB
+    {
+        public static string server = "";
+        public static string username = "";
+        public static string port = "";
+        public static string password = "";
+        public static string database = "";
+
+    }
+}
